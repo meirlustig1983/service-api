acbbf5397f5424c9ff560814ac16a2258a732d37
meirlustig <meirlustig1983@gmail.com>
2023-04-20 20:11:32 -0700

    FIX-001 fix 'ServiceApiApplicationTests'

--- src/test/java/com/ml/serviceapi/ServiceApiApplicationTests.java
+++ src/test/java/com/ml/serviceapi/ServiceApiApplicationTests.java
@@ -1,13 +1,24 @@
 package com.ml.serviceapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ServiceApiApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @Test
+    public void healthTest() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/serviceapi/helloWorld", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo("Hello, World!");
+    }
 }
